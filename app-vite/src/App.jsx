f5b1d0dd898905fd4da304206cd776c39ec50142
ginpei <ginpei@gmail.com>
2022-01-02 00:50:17 +0000

    import sub packages over npm-link

--- app-vite/src/App.jsx
+++ app-vite/src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { useOne } from "@ginpei/x--react-vs-monorepo--sub";
 
 function App() {
   const [count, setCount] = useState(0)
+  const one = useOne();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
+        <p>
+          One: {one}
+        </p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
